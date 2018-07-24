cbe59a8ad15b7db6535a08da368e52cf1ce2adb0
Peter <youngenli@gmail.com>
2018-07-24 15:55:49 -0500

    initial files

---
+++ SuperHeroes/Controllers/HeroController.cs
@@ -0,0 +1,44 @@
+﻿using SuperHeroes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroes
+{
+    public class HeroController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Hero
+        public ActionResult Index()
+        {
+            return View(db.superHeroes.ToList());
+        }
+
+        public ActionResult Create() {
+            ViewBag.HeroID = new SelectList(db.superHeroes, "Super ID", "Name");
+            return View();
+        }
+
+        [AcceptVerbs]
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="Name, Alter Ego, Ability, Secondary Ability, Catchphrase")] SuperHero superHero) {
+            if (ModelState.IsValid) {
+                db.superHeroes.Add(superHero);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(superHero);
+        }
+
+        public ActionResult Delete(SuperHero superHero) {
+            SuperHero hero = (from h in db.superHeroes
+                              where h.HeroId == superHero.HeroId
+                              select h).FirstOrDefault();
+            db.superHeroes.Remove(hero);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SuperHeroes/Models/SuperHero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int HeroId { get; set; }
+        public string HeroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string heroPrimaryAbility { get; set; }
+        public string HeroSecondaryAbility { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+}
